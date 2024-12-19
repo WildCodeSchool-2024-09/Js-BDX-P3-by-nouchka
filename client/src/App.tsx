3398ae2926e3b2a755c0fe79786343fd97983790
Ccilpy <cecilepuyol@gmail.com>
2024-12-19 13:20:26 +0100

    add variables CSS and images and logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-    <section>
-      <h1>by.Nouchka</h1>
-      <img src="./src/assets/images/IMG_2711.png" alt="" />
+      <section>
+        <h1>by.Nouchka</h1>
+        <img src="./src/assets/images/IMG_2711.png" alt="" />
       </section>
     </>
   );
