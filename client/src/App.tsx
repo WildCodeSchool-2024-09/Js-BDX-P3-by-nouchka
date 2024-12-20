eb53311aa82e7ad656fe60d18d8c01a680e4eb5b
Ccilpy <cecilepuyol@gmail.com>
2024-12-20 13:57:47 +0100

    fix fonts and hgroup tag

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <section>
+      <hgroup>
         <h1>by.Nouchka</h1>
         <img src="./src/assets/images/IMG_2711.png" alt="" />
-      </section>
+      </hgroup>
     </>
   );
 }
