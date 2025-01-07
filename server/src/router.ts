b43b5d52c92428980e309743a4be1fd808863d7a
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2025-01-07 21:07:08 +0100

    create Clients_Module and its paths

--- server/src/router.ts
+++ server/src/router.ts
@@ -13,6 +13,13 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+import clientsActions from "./modules/clients/clientsActions";
+
+router.get("/api/clients", clientsActions.browse);
+router.get("api/clients/:id", clientsActions.read);
+router.put("/api/clients/:id", clientsActions.edit);
+router.post("/api/clients", clientsActions.add);
+router.delete("/api/clients/:id", clientsActions.destroy);
 /* ************************************************************************* */
 
 export default router;
