88e7323045fb847d1f3865e0e4c9af3db2de9a9c
manon labat <manonlabat10@gmail.com>
2025-01-09 08:43:19 +0100

    contact-block

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
 import ContactBlock from "../src/components/contact-block/index";
+import Header from "./components/Header";
 
 function App() {
   return (
