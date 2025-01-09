ba75c18c05f343e0b48a9ef859ba7497aa0da547
PIERRE DE CASTELBAJAC <decastelbajacpierre@gmail.com>
2025-01-09 11:02:22 +0100

    router added

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,12 +7,23 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
+import adminActions from "./modules/item/Event-photo/adminActions";
+import eventActions from "./modules/item/Event-photo/eventActions";
 import itemActions from "./modules/item/itemActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+router.get("/api/admins", adminActions.browse);
+router.get("/api/admins/:id", adminActions.read);
+router.post("/api/admins", adminActions.add);
+
+router.get("/api/events", eventActions.browse);
+router.get("/api/events/:id", eventActions.read);
+router.post("/api/events", eventActions.add);
+router.put("/api/events/:id", eventActions.edit);
+router.delete("/api/events/:id", eventActions.destroy);
 /* ************************************************************************* */
 
 export default router;
