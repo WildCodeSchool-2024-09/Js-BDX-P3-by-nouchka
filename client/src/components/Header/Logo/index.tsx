d54e6838850a06b258db1597df4ce7f3d39fb601
Ccilpy <cecilepuyol@gmail.com>
2025-01-09 17:20:43 +0100

    update header with new logo

--- client/src/components/Header/Logo/index.tsx
+++ client/src/components/Header/Logo/index.tsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 export default function Logo() {
   return (
     <Link to="/" className="logo-link">
-      <img src="../src/assets/logo/logo.svg" alt="retour à la page d'accueil" />
+      <img
+        src="../src/assets/logo/new_logo.svg"
+        alt="retour à la page d'accueil"
+      />
     </Link>
   );
 }
