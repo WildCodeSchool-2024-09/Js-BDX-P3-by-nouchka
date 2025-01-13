08f85b961859fefac3d251a8db93802952792dac
manon labat <manonlabat10@gmail.com>
2025-01-13 13:27:22 +0100

    fix_contact-block

--- client/src/components/contact-block/index.tsx
+++ client/src/components/contact-block/index.tsx
@@ -20,47 +20,46 @@ export default function ContactBlock() {
 
   return (
     <>
-    <h2 className="contact">Me Contacter</h2>
-    <form action="sumbit" className="contactForm">
-      
-      <p className="contactDescription">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum
-        necessitatibus reprehenderit molestias officiis amet culpa tempora
-        asperiores cupiditate debitis, quia vero, alias dignissimos consectetur
-        ducimus, doloribus fugiat maxime fugit?
-      </p>
-      <label className="Name">
-      <input
-        className="blockName"
-        required
-        type="text"
-        name="firstName"
-        placeholder="Nom, Prénom..."
-      />
-      </label>
-      <label className="Email">
-      <input
-        className="blockEmail"
-        required
-        type="email"
-        name="email"
-        value={mail}
-        placeholder="Votre e-mail..."
-        onChange={handleChange}
-      />
-      </label>
-      {emailError && <p className="errorEmail">{emailError}</p>}
-      <label className="Message">
-      <textarea
-        className="blockMessage"
-        name="message"
-        placeholder="Votre message..."
-      />
-      </label>
-      <button className="send" type="submit">
-        Envoyer
-      </button>
-    </form>
+      <h2 className="contact">Me Contacter</h2>
+      <form action="sumbit" className="contactForm">
+        <p className="contactDescription">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum
+          necessitatibus reprehenderit molestias officiis amet culpa tempora
+          asperiores cupiditate debitis, quia vero, alias dignissimos
+          consectetur ducimus, doloribus fugiat maxime fugit?
+        </p>
+        <label className="Name">
+          <input
+            className="blockName"
+            required
+            type="text"
+            name="firstName"
+            placeholder="Nom, Prénom..."
+          />
+        </label>
+        <label className="Email">
+          <input
+            className="blockEmail"
+            required
+            type="email"
+            name="email"
+            value={mail}
+            placeholder="Votre e-mail..."
+            onChange={handleChange}
+          />
+        </label>
+        {emailError && <p className="errorEmail">{emailError}</p>}
+        <label className="Message">
+          <textarea
+            className="blockMessage"
+            name="message"
+            placeholder="Votre message..."
+          />
+        </label>
+        <button className="send" type="submit">
+          Envoyer
+        </button>
+      </form>
     </>
   );
 }
