fa817cc2bc668652ddb071277d80230b6973e9c2
manon labat <manonlabat10@gmail.com>
2025-01-16 12:10:45 +0100

    re-commit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -13,7 +13,6 @@ import Upcycling from "./pages/Upcycling";
 
 // Import the main app component
 import App from "./App";
-import RegisteurBlock from "./components/Registeur";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
