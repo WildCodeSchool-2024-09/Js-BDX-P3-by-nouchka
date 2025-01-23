5779fa72533c00d75876d48717f754d909ea91b5
Makenchii <marwane422@hotmail.fr>
2025-01-23 11:30:40 +0100

    finish no style

--- client/src/components/ImageHero/ImageHero.tsx
+++ client/src/components/ImageHero/ImageHero.tsx
@@ -1,28 +1,25 @@
+import { useEffect } from "react";
 import "./ImageHero.css";
 
 interface ImageHeroProps {
-  imageUrl?: string; // Permet de surcharger l'image via les props
-  altText: string; // Texte alternatif obligatoire
-  title?: string; // Permet de surcharger le titre via les props
-  subtitle?: string; // Permet de surcharger le sous-titre via les props
+  imageUrl?: string;
+  altText: string;
+  title?: string;
+  subtitle?: string;
 }
 
-function ImageHero({ imageUrl, altText, title, subtitle }: ImageHeroProps) {
+function ImageHero({ imageUrl, title, subtitle }: ImageHeroProps) {
+  useEffect(() => {
+    const root = document.documentElement;
+    if (imageUrl) {
+      root.style.setProperty("--hero-image", `url(${imageUrl})`);
+    }
+  }, [imageUrl]);
+
   return (
-    <header
-      className="image-hero"
-      style={{
-        backgroundImage: `url(${imageUrl || "var(--hero-image)"})`,
-      }}
-    >
-      {/* L'image cachée pour l'accessibilité */}
-      <img
-        src={imageUrl || "var(--hero-image)"}
-        alt={altText}
-        style={{ display: "none" }}
-      />
-      {title && <h1 className="image-hero-title">{title}</h1>}
-      {subtitle && <p className="image-hero-subtitle">{subtitle}</p>}
+    <header className="image-hero">
+      <h1 className="image-hero-title">{title}</h1>
+      <p className="image-hero-subtitle">{subtitle}</p>
     </header>
   );
 }
