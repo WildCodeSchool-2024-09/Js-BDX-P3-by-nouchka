454bc209d83a8470f48893884381e2583b529db3
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2025-01-27 14:14:01 +0100

    adding likes on clients module

--- client/src/components/Cart/index.tsx
+++ client/src/components/Cart/index.tsx
@@ -1,42 +1,11 @@
 import "./style.css";
 
-import { useEffect, useState } from "react";
-import type { Jewelry } from "../../types/jewelry";
-
 export default function Cart() {
-  const [cartItems, setCartItems] = useState<Jewelry[]>([]);
-
-  useEffect(() => {
-    const cart = JSON.parse(localStorage.getItem("cart") || "[]");
-    setCartItems(cart);
-  }, []);
-  function deleteProduct(id: number): void {
-    const updatedCart = cartItems.filter((item) => item.id !== id);
-    localStorage.setItem("cart", JSON.stringify(updatedCart));
-    setCartItems(updatedCart);
-  }
   return (
-    <>
-      <h1 className="cart">Articles</h1>
-      {cartItems.map((item) => (
-        <section className="cartProduct" key={item.id}>
-          <h2 className="productTitle">{item.name}</h2>
-          <img className="productImage" src={item.URL} alt={item.name} />
-          <p className="">{item.price} €</p>
-          <p>Quantité: {item.quantity}</p>
-          <button
-            type="button"
-            className="garbageButton"
-            onClick={() => deleteProduct(item.id)}
-          >
-            <img
-              src="public/garbage.svg"
-              alt="poubelle"
-              className="garbage-img"
-            />
-          </button>
-        </section>
-      ))}
-    </>
+    <section className="cart">
+      <h2>Votre Panier</h2>
+      <img src="../src/assets/cart/cart.svg" alt="" />
+      <p>Votre panier est vide.</p>
+    </section>
   );
 }
