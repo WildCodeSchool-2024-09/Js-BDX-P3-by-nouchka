8b07fffb7f5f7f410756b0b8a3c2daa5234a5d33
manon labat <manonlabat10@gmail.com>
2025-01-28 10:53:14 +0100

    fix-registration and token

--- server/src/router.ts
+++ server/src/router.ts
@@ -9,12 +9,6 @@ const router = express.Router();
 
 // Define item-related routes
 
-import itemActions from "./modules/item/itemActions";
-
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
-
 import clientsActions from "./modules/clients/clientsActions";
 
 router.get("/api/clients", clientsActions.browse);
@@ -27,9 +21,6 @@ import jewelryActions from "./modules/jewelry/jewelryActions";
 
 router.get("/api/jewelry", jewelryActions.browse);
 router.get("/api/jewelry/:id", jewelryActions.read);
-router.post("/api/jewelry", jewelryActions.add);
-router.put("/api/jewelry/:id", jewelryActions.edit);
-router.delete("/api/jewelry/:id", jewelryActions.destroy);
 
 /* ************************************************************************* */
 
@@ -38,27 +29,31 @@ import adminActions from "./modules/admin/adminActions";
 router.get("/api/admins", adminActions.browse);
 router.get("/api/admins/:id", adminActions.read);
 router.post("/api/admins", authMiddleware.hashPassword, adminActions.add);
-router.put("/api/admins/:id", adminActions.edit);
-router.delete("/api/admins/:id", adminActions.destroy);
 
 import pagesActions from "./modules/pages/pagesActions";
 
 router.get("/api/pages", pagesActions.browse);
 router.get("/api/pages/:name", pagesActions.read);
-router.put("/api/pages/:name", pagesActions.edit);
 
 import eventActions from "./modules/event/eventActions";
 
 router.get("/api/events", eventActions.browse);
 router.get("/api/events/:id", eventActions.read);
-router.post("/api/events", eventActions.add);
-router.put("/api/events/:id", eventActions.edit);
-router.delete("/api/events/:id", eventActions.destroy);
 
 import authMiddleware from "../src/Middleware/authMiddleware";
 
 router.post("/api/auth/login", authMiddleware.login);
 
 router.use(authMiddleware.verifyToken);
+
+router.put("/api/pages/:name", pagesActions.edit);
+router.post("/api/events", eventActions.add);
+router.put("/api/events/:id", eventActions.edit);
+router.delete("/api/events/:id", eventActions.destroy);
+router.post("/api/jewelry", jewelryActions.add);
+router.put("/api/jewelry/:id", jewelryActions.edit);
+router.delete("/api/jewelry/:id", jewelryActions.destroy);
+router.put("/api/admins/:id", adminActions.edit);
+router.delete("/api/admins/:id", adminActions.destroy);
 
 export default router;
