af0bdb837257738bfb479ce4f589c7017132d2aa
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2025-01-29 09:35:45 +0100

    fixing errors

--- client/src/pages/privacy-policy/PrivacyPolicy.tsx
+++ client/src/pages/privacy-policy/PrivacyPolicy.tsx
@@ -1,204 +1,198 @@
 export default function PrivacyPolicy() {
   return (
-    <>
-      <section className="legalmentions-container">
-        <h2 className="secondary-title">
-          Politique de Confidentialité de by.Nouchka{" "}
-        </h2>
-        <p className="legalMentions-text">
-          Dernière mise à jour : 19/01/2025 by.Nouchka accorde une grande
-          importance à la protection des données personnelles de ses clients.
-          Cette politique explique comment nous collectons, utilisons, stockons
-          et protégeons vos informations personnelles lorsque vous utilisez
-          notre site internet (ci-après "le Site").
+    <section className="legalmentions-container">
+      <h2 className="secondary-title">
+        Politique de Confidentialité de by.Nouchka{" "}
+      </h2>
+      <p className="legalMentions-text">
+        Dernière mise à jour : 19/01/2025 by.Nouchka accorde une grande
+        importance à la protection des données personnelles de ses clients.
+        Cette politique explique comment nous collectons, utilisons, stockons et
+        protégeons vos informations personnelles lorsque vous utilisez notre
+        site internet (ci-après "le Site").
+      </p>
+      <p className="legalMentions-text">
+        1. Responsable du traitement des données Le responsable du traitement
+        des données est :
+        <ul>
+          {" "}
+          <li>
+            Nom de l’entreprise : by.Nouchka Adresse : 3 cours de l’intendance
+            33000 Bordeaux.
+          </li>{" "}
+          <li> E-mail : nina.rch16@gmail.com Numéro SIRET : 93801667200017.</li>
+        </ul>
+      </p>
+      <p className="legalMentions-text">
+        2. Données collectées.{" "}
+        <p>
+          Nous collectons uniquement les données nécessaires pour traiter vos
+          commandes et améliorer votre
         </p>
-        <p className="legalMentions-text">
-          1. Responsable du traitement des données Le responsable du traitement
-          des données est :
-          <ul>
-            {" "}
-            <li>
-              Nom de l’entreprise : by.Nouchka Adresse : 3 cours de l’intendance
-              33000 Bordeaux.
-            </li>{" "}
-            <li>
-              {" "}
-              E-mail : nina.rch16@gmail.com Numéro SIRET : 93801667200017.
-            </li>
-          </ul>
-        </p>
-        <p className="legalMentions-text">
-          2. Données collectées.{" "}
-          <p>
-            Nous collectons uniquement les données nécessaires pour traiter vos
-            commandes et améliorer votre
-          </p>
-          expérience sur le Site. Ces données incluent :
-          <ul>
-            <li>
-              Données personnelles fournies directement par vous :
-              <ul>
-                <li>Nom et prénom </li> <li>Adresse e-mail </li>{" "}
-                <li>Adresse postale </li> <li>Numéro de téléphone </li>
-                <li>
-                  {" "}
-                  Informations de paiement (via une passerelle sécurisée, sans
-                  conservation par nos soins)
-                </li>
-              </ul>
-            </li>
-            <li>
-              Données collectées automatiquement :
-              <ul>
-                <li>Adresse IP</li>
-                <li>
-                  Données de navigation (pages visitées, temps passé, etc.).
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <p className="legalMentions-text">
-            {" "}
-            Finalités du traitement des données
-            <p>
-              Vos données sont collectées pour :
-              <ol>
-                <li>
-                  La gestion des commandes : Traitement, expédition, facturation
-                  et suivi des commandes.
-                </li>
-                <li>
-                  Le service client : Répondre à vos demandes, traiter vos
-                  retours ou réclamations.
-                </li>
-                <li>
-                  Le marketing :
-                  <ul>
-                    <li>
-                      Envoi d’offres promotionnelles, si vous avez donné votre
-                      consentement pour recevoir notre newsletter.
-                    </li>
-                    <li>Personnalisation de votre expérience sur le Site.</li>
-                  </ul>
-                </li>
-              </ol>
-            </p>
-          </p>
-        </p>
-        <p className="legalMentions-text">
-          L’analyse et l’amélioration du Site :{" "}
-          <p>
-            Analyse des comportements pour améliorer nos services et détecter
-            d’éventuelles anomalies techniques.
-          </p>
-        </p>
-        <p className="legalMentions-text">
-          4. Partage des données
-          <p>
-            Nous ne partageons vos données personnelles qu’avec des tiers de
-            confiance et uniquement lorsque cela est nécessaire :
+        expérience sur le Site. Ces données incluent :
+        <ul>
+          <li>
+            Données personnelles fournies directement par vous :
             <ul>
+              <li>Nom et prénom </li> <li>Adresse e-mail </li>{" "}
+              <li>Adresse postale </li> <li>Numéro de téléphone </li>
               <li>
-                <ul>
-                  <li>Fournisseurs de services tiers :</li>
-                  <li>
-                    Prestataires logistiques et de livraison (ex. :
-                    transporteurs).
-                    <li>Plateformes de paiement sécurisées.</li>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                Obligations légales : Nous pouvons divulguer vos données si la
-                loi l'exige (ex. : fraude, demande judiciaire).
+                {" "}
+                Informations de paiement (via une passerelle sécurisée, sans
+                conservation par nos soins)
               </li>
             </ul>
+          </li>
+          <li>
+            Données collectées automatiquement :
+            <ul>
+              <li>Adresse IP</li>
+              <li>
+                Données de navigation (pages visitées, temps passé, etc.).
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <p className="legalMentions-text">
+          {" "}
+          Finalités du traitement des données
+          <p>
+            Vos données sont collectées pour :
+            <ol>
+              <li>
+                La gestion des commandes : Traitement, expédition, facturation
+                et suivi des commandes.
+              </li>
+              <li>
+                Le service client : Répondre à vos demandes, traiter vos retours
+                ou réclamations.
+              </li>
+              <li>
+                Le marketing :
+                <ul>
+                  <li>
+                    Envoi d’offres promotionnelles, si vous avez donné votre
+                    consentement pour recevoir notre newsletter.
+                  </li>
+                  <li>Personnalisation de votre expérience sur le Site.</li>
+                </ul>
+              </li>
+            </ol>
           </p>
         </p>
-        <p className="legalMentions-text">
-          5. Conservation des données
-          <p>Vos données personnelles sont conservées :</p>
+      </p>
+      <p className="legalMentions-text">
+        L’analyse et l’amélioration du Site :{" "}
+        <p>
+          Analyse des comportements pour améliorer nos services et détecter
+          d’éventuelles anomalies techniques.
+        </p>
+      </p>
+      <p className="legalMentions-text">
+        4. Partage des données
+        <p>
+          Nous ne partageons vos données personnelles qu’avec des tiers de
+          confiance et uniquement lorsque cela est nécessaire :
           <ul>
             <li>
-              Pour les commandes : Pendant toute la durée de la relation
-              commerciale, puis archivées pour la durée légale (ex. : 5 ans pour
-              les factures).
+              <ul>
+                <li>Fournisseurs de services tiers :</li>
+                <li>
+                  Prestataires logistiques et de livraison (ex. :
+                  transporteurs).
+                  <li>Plateformes de paiement sécurisées.</li>
+                </li>
+              </ul>
             </li>
             <li>
-              Pour les finalités marketing : Jusqu’à ce que vous retiriez votre
-              consentement (désinscription possible à tout moment).
-            </li>
-            <li>Pour les données de navigation : Jusqu’à 13 mois maximum.</li>
-          </ul>
-        </p>
-        <p className="legalMentions-text">
-          6. Vos droits
-          <p>Conformément au RGPD, vous disposez des droits suivants :</p>
-          <ul>
-            <li>
-              Droit d’accès : Consulter les données personnelles que nous
-              détenons sur vous.
-            </li>
-            <li>
-              Droit de rectification : Modifier ou mettre à jour vos données.
-            </li>
-            <li>
-              Droit à l’effacement : Demander la suppression de vos données,
-              sauf si leur conservation est requise par la loi.
-            </li>
-            <li>
-              Droit d’opposition : Refuser le traitement de vos données pour des
-              finalités spécifiques (marketing, etc.).
-            </li>
-            <li>
-              Droit à la portabilité : Recevoir vos données dans un format
-              structuré et lisible, ou demander leur transmission à un tiers.
-            </li>
-            <li>
-              Droit de retirer votre consentement : À tout moment, pour les
-              traitements nécessitant votre accord (ex. : newsletter).
-              <p>
-                {" "}
-                Pour exercer vos droits, contactez-nous à l’adresse suivante :{" "}
-                [votre adresse e-mail dédiée].
-              </p>
+              Obligations légales : Nous pouvons divulguer vos données si la loi
+              l'exige (ex. : fraude, demande judiciaire).
             </li>
           </ul>
         </p>
-        <p className="legalMentions-text">
-          7. Sécurité des données
-          <p>
-            Nous mettons en œuvre des mesures techniques et organisationnelles
-            pour protéger vos données contre tout accès non autorisé, perte ou
-            altération :
-          </p>
-          <ul>
-            <li>Hébergement sécurisé du Site.</li>
-            <li>
-              Cryptage des données de paiement via des passerelles sécurisées
-              (ex. : SSL/TLS).
-            </li>
-            <li>
-              Accès limité aux données personnelles par les employés autorisés.
-            </li>
-          </ul>
+      </p>
+      <p className="legalMentions-text">
+        5. Conservation des données
+        <p>Vos données personnelles sont conservées :</p>
+        <ul>
+          <li>
+            Pour les commandes : Pendant toute la durée de la relation
+            commerciale, puis archivées pour la durée légale (ex. : 5 ans pour
+            les factures).
+          </li>
+          <li>
+            Pour les finalités marketing : Jusqu’à ce que vous retiriez votre
+            consentement (désinscription possible à tout moment).
+          </li>
+          <li>Pour les données de navigation : Jusqu’à 13 mois maximum.</li>
+        </ul>
+      </p>
+      <p className="legalMentions-text">
+        6. Vos droits
+        <p>Conformément au RGPD, vous disposez des droits suivants :</p>
+        <ul>
+          <li>
+            Droit d’accès : Consulter les données personnelles que nous détenons
+            sur vous.
+          </li>
+          <li>
+            Droit de rectification : Modifier ou mettre à jour vos données.
+          </li>
+          <li>
+            Droit à l’effacement : Demander la suppression de vos données, sauf
+            si leur conservation est requise par la loi.
+          </li>
+          <li>
+            Droit d’opposition : Refuser le traitement de vos données pour des
+            finalités spécifiques (marketing, etc.).
+          </li>
+          <li>
+            Droit à la portabilité : Recevoir vos données dans un format
+            structuré et lisible, ou demander leur transmission à un tiers.
+          </li>
+          <li>
+            Droit de retirer votre consentement : À tout moment, pour les
+            traitements nécessitant votre accord (ex. : newsletter).
+            <p>
+              {" "}
+              Pour exercer vos droits, contactez-nous à l’adresse suivante :{" "}
+              [votre adresse e-mail dédiée].
+            </p>
+          </li>
+        </ul>
+      </p>
+      <p className="legalMentions-text">
+        7. Sécurité des données
+        <p>
+          Nous mettons en œuvre des mesures techniques et organisationnelles
+          pour protéger vos données contre tout accès non autorisé, perte ou
+          altération :
         </p>
-        <p className="legalMentions-text">
-          8. Modifications de la Politique de Confidentialité
-          <p>
-            Nous nous réservons le droit de modifier cette politique à tout
-            moment pour rester conformes aux évolutions légales ou techniques.
-            Toute modification sera communiquée via une notification sur le
-            Site.
-          </p>
-          <p>9. Contact</p>
-          <p>
-            Pour toute question ou préoccupation concernant cette Politique de
-            Confidentialité, vous pouvez nous contacter via le site internet
-            directement.
-          </p>
+        <ul>
+          <li>Hébergement sécurisé du Site.</li>
+          <li>
+            Cryptage des données de paiement via des passerelles sécurisées (ex.
+            : SSL/TLS).
+          </li>
+          <li>
+            Accès limité aux données personnelles par les employés autorisés.
+          </li>
+        </ul>
+      </p>
+      <p className="legalMentions-text">
+        8. Modifications de la Politique de Confidentialité
+        <p>
+          Nous nous réservons le droit de modifier cette politique à tout moment
+          pour rester conformes aux évolutions légales ou techniques. Toute
+          modification sera communiquée via une notification sur le Site.
         </p>
-      </section>
-    </>
+        <p>9. Contact</p>
+        <p>
+          Pour toute question ou préoccupation concernant cette Politique de
+          Confidentialité, vous pouvez nous contacter via le site internet
+          directement.
+        </p>
+      </p>
+    </section>
   );
 }
