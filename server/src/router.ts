417ccb88bd304cdc68cb1c104ed4c56f7c243621
Makenchii <marwane422@hotmail.fr>
2025-01-29 10:18:25 +0100

    m

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,12 +8,6 @@ const router = express.Router();
 
 // Define item-related routes
 
-import itemActions from "./modules/item/itemActions";
-
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
-
 import clientsActions from "./modules/clients/clientsActions";
 
 router.get("/api/clients", clientsActions.browse);
