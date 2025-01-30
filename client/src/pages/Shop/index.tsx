f0f50a57f2fb9f791de1159191ec63d79dc5ea4f
manon labat <manonlabat10@gmail.com>
2025-01-30 16:32:33 +0100

    create Product page

--- client/src/pages/Shop/index.tsx
+++ client/src/pages/Shop/index.tsx
@@ -1,16 +1,9 @@
-import { Link } from "react-router-dom";
 import "../../App.css";
 
 export default function Shop() {
- 
-
   return (
     <>
       <h2>Shop by.Nouchka</h2>
-      <Link to={`/jewelry/1`}>Product</Link>
-      <Link to={`/jewelry/2`}>Product</Link>
-      <Link to={`/jewelry/3`}>Product</Link>
-      <Link to={`/jewelry/4`}>Product</Link>
     </>
   );
 }
